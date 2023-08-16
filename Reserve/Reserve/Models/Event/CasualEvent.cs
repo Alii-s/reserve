8505c2fe241121fad4f18c6e357f8e3311b2ba5e
Yehia Tarek Ragab <94568731+yehiatarek63@users.noreply.github.com>
2023-08-16 20:14:16 +0300

    Added Models

---
+++ Reserve/Reserve/Models/Event/CasualEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reserve.Models.Event;
+
+public class CasualEvent
+{
+    public Guid Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string OrganizerName { get; set; }
+    [Required]
+    public string OrganizerEmail { get; set; }
+    [Required]
+    public int MaximumCapacity { get; set; }
+    [Required]
+    public string Location { get; set; }
+    [Required]
+    public DateTime StartDate { get; set; }
+    [Required]
+    public DateTime EndDate { get; set; }
+    [Required]
+    public List<string> Tags { get; set; }
+    [Required]
+    public int CurrentCapacity { get; set; }
+}
---
+++ Reserve/Reserve/Models/Event/CasualTicket.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reserve.Models.Event;
+
+public class CasualTicket
+{
+    public Guid Id { get; set; }
+    [Required]
+    public string ReserverName { get; set; }
+    [Required]
+    public string ReserverEmail { get; set; }
+    [Required]
+    public Guid CasualEventId { get; set; }
+
+}
